445f9a200fa70d3dd50ec254515184301c5c08c7
Apoptoz <davidq@tcd.ie>
2017-09-09 23:00:37 +0100

    Ajout de l'initi

--- substrat.py
+++ substrat.py
@@ -1,5 +1,12 @@
+
+
 class Substrat:
 
-	def __init__(self, tableau):
-		self.tableau = tableau
-		
+	def __init__(self, contenu,coord_x,coord_y):
+		self.contenu = contenu #Liste du contenu de la case
+		self.coord_x = coord_x
+		self.coord_y = coord_y
+
+
+
+n = 20 #Taille du tableau
