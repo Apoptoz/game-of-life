e5d28871e7bc7f9a6c6ea6ebfd9bc8c4eca06af4
Apoptoz <davidq@tcd.ie>
2017-09-09 22:10:48 +0100

    Ajout de strcture

--- cellule.py
+++ cellule.py
@@ -1,6 +1,9 @@
 
 
 class Cellule:
-    def __init__(self,age,energy):
+    def __init__(self,colour,age,energy,coord_x,coord_y):
         self.age = age
         self.energy = energy
+        self.colour = colour
+        self.coord_x = coord_x
+        self.coord_y = coord_y
