d78a9f34cb59143174898c47a2f69f7c1ab39f72
Apoptoz <davidq@tcd.ie>
2017-09-10 12:05:01 +0100

    Ajout du fichier main, pour l'interface graphique

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.locals import *
+
+
+if __name__ == '__main__':
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((600,400))
+    pygame.display.set_caption('Flo is Life,Quentin is Game')
+    
+
+
+
+
+    quit()
