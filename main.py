23f50760616b4e8dd611ecd47de9c47781e8efac
Apoptoz <davidq@tcd.ie>
2017-09-10 12:27:13 +0100

    Bidouillage de pygame

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+import os
 import pygame
 from pygame.locals import *
 
@@ -6,9 +9,25 @@ if __name__ == '__main__':
 
     pygame.init()
 
+    clock = pygame.time.Clock()
+
     screen = pygame.display.set_mode((600,400))
     pygame.display.set_caption('Flo is Life,Quentin is Game')
-    
+    background = pygame.Surface(screen.get_size())
+
+    if pygame.font:
+        font = pygame.font.Font(None,36)
+        text = font.render("F&Q's Game of Life",1,(10,10,10))
+        textpos = text.get_rect(centerx=background.get_width()/2)
+        background.blit(text,textpos)
+
+
+    grid = pygame.draw.rect(background,(255,255,255),(20,20,560,360),1)
+    screen.blit(background,(0,0))
+    pygame.display.flip()
+
+    while 1:
+        clock.tick(500)
 
 
 
